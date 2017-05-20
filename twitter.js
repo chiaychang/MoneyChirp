b5ff7ad3755575770e677d0d3d1e59d49e62f87a
Joy Chang <joychang@Joyteki-MacBook-Pro.local>
2017-05-20 12:41:15 -0500

    more twitter score

--- twitter.js
+++ twitter.js
@@ -8,60 +8,85 @@ var client = new Twitter(accountInfo);
 var companies = [{
     name: 'Apple',
     handle: 'Apple',
-    reach: 0
+    score: 0
 }, {
     name: 'Google',
     hadle: 'Google',
-    reach: 0
+    score: 0
 }, {
     name: 'Facebook',
     handle: 'facebook',
-    reach: 0
+    score: 0
+
 }];
 
+var scoreArray = [];
 
+// function trend(totalPostReach) {
+//     companies[i].reach = totalPostReach;
+//     console.log(companies[i].reach);
+// }
 
 function getTweets() {
-
+ 
     for (var i = 0; i < companies.length; i++) {
 
-        var params = { q: '%40' + companies[i].name, count: 3, lang: 'en' };
 
-        client.get('search/tweets', params, function(error, response) {
+        var score = 0;
+        var params = { q: '%40' + companies[i].name, count: 3, lang: 'en'};
+
+        client.get('search/tweets', params,function(error, response) {
             if (error) {
                 console.log('Error occurred: ' + error);
             } else if (!error) {
 
 
-                console.log(response);
-                console.log(response.statuses.length);
+              
+               // console.log(response.statuses.length);
 
-                var totalPostReach = 0;
-
+                
                 for (j = 0; j < response.statuses.length; j++) {
+
+                	// console.log(response.statuses[j].retweet_count, response.statuses[j].favorite_count);
                     var postReach = (response.statuses[j].retweet_count +
                         response.statuses[j].favorite_count);
 
-                    console.log(postReach);
-                    totalPostReach += postReach;
-
-
+                    score += postReach;
+          
                 }
-                // console.log(companies[i].reach);
-                // companies[i].reach = totalPostReach;
-                // console.log(companies[i].reach);
 
-                // console.log(response);
-                // for (var i = 0; i < tweets.length; i++) {
-                //     console.log("#" + (1 + i) + ": " + tweets[i].text);
-                // }
+               // scoreArray.push(score);
+               // console.log(scoreArray);
+           
+            
 
+               //companies[params.index].reach = totalPostReach;
+     
             }
+           
         });
 
-
-
+    
+       // totalPostReach = companies[i].reach;
+         // console.log(i);
     }
+
+
 }
 
+function assignScore (companies,scoreArray){
+	
+	console.log(companies);
+	
+	for(var k = 0; k< companies.length; k++){
+	    console.log(scoreArray[k]); 
+		// console.log(companies[k]);
+	}
+
+}
+
+
 getTweets();
+// console.log(companies.length);
+assignScore(companies, scoreArray);
+
