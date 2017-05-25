6cc6ccc7039c03ddc6336bb91b1c57b8f9731b73
Joy Chang <joychang@joyteki-mbp.attlocal.net>
2017-05-24 20:02:59 -0500

    connecting Twitter API data and Chart.js

--- twitter.js
+++ twitter.js
@@ -1,43 +1,82 @@
+// for testing, this part should be deleted once Sequelize is connected 
+// Require mysql
+var fs = require("fs");
+var mysql = require("mysql");
+
+
+// Set up our connection information
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "MoneyChirp_db"
+    });
+}
+// Connect to the database
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// --------------------------------------------------
+
+// var fs = require("fs");
+// var twitter = require("./twitter.js");
+
+// twitter.twitter.getParams();
+
+
 var keys = require("./keys.js");
-var accountInfo = keys.twitterKeys;
+var accountInfo = keys.twierKeys;
 
 var Twitter = require('twitter');
 
 var client = new Twitter(accountInfo);
 
+var companyArray = [];
+var handleArray = [];
 var scoreArray = [];
+
 var twitter = {
-    companies: [{
-        name: 'Apple',
-        handle: 'Apple',
-        score: null
-    }, {
-        name: 'Google',
-        hadle: 'Google',
-        score: null
-    }, {
-        name: 'Facebook',
-        handle: 'facebook',
-        score: null
-    }],
+    getParams: function() {
 
-    companiesArray: [],
 
+        connection.query("Select * from companies", function(err, res) {
+
+            if (err) {
+                throw err;
+            }
+            console.log(res);
+            for (var z = 0; z < res.length; z++) {
+                companyArray.push(res[z].name);
+                handleArray.push(res[z].handle);
+
+            }
+            console.log(companyArray, handleArray);
+            twitter.getTweets();
+        });
+    },
 
     getTweets: function() {
 
-        for (var i = 0; i < this.companies.length; i++) {
+        for (var i = 0; i < handleArray.length; i++) {
 
-           
-
-            var params = { q: '%40' + this.companies[i].name, count: 3, lang: 'en', index: i };
+            var params = { q: '%40' + handleArray[i], count: 3, lang: 'en', index: i };
 
             client.get('search/tweets', params, function(error, response) {
                 if (error) {
                     console.log('Error occurred: ' + error);
                 } else if (!error) {
 
-                   var trendingScore = 0;
+                    var trendingScore = 0;
                     console.log(response);
 
                     for (j = 0; j < response.statuses.length; j++) {
@@ -47,23 +86,18 @@ var twitter = {
                             response.statuses[j].favorite_count);
                         console.log(postReach);
                         trendingScore += postReach;
-                      
 
                     }
 
-                      console.log(trendingScore);
-                      scoreArray.push(trendingScore);
-                    // scoreArray.push(trendingScore);
+                    console.log(trendingScore);
+                    scoreArray.push(trendingScore);
                     console.log(scoreArray);
-
                 }
-
             });
-
         }
 
-    },
+
+    }
 }
 
-twitter.getTweets();
-
+module.exports= twitter;
